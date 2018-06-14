4d4b7bb09095118b86eddc3f700e3c0466b81afc
Fernando Reyes (likewhoa) <design@missionaccomplish.com>
2018-06-14 23:45:17 +0000

    Testing new method 3

--- server/controllers/contact_list_controller.js
+++ server/controllers/contact_list_controller.js
@@ -96,7 +96,7 @@ exports.sendConfirmation = (req, res, next) => {
 		}
 
 		if (response.statusCode >= 200 && response.statusCode < 300) {
-                        res.sendFile('/home/teah/htdocs/foobar.html');
+                        window.location.replace("https://teah.weboperative.com");
 			//res.sendFile(path.join(__dirname, '../static/check-inbox.html'));
 		} else {
 			res.sendFile(path.join(__dirname, '../static/error.html'));
