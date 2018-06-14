1412d9b649e3444c13aa41232164037207770060
Fernando Reyes (likewhoa) <design@missionaccomplish.com>
2018-06-14 20:16:55 +0000

    BBS settings

--- settings.js
+++ settings.js
@@ -1,17 +1,17 @@
 // Change the url to the domain of your app
 exports.url = 'https://blackboxstocks-sendgrid.herokuapp.com';
 
-exports.senderEmail = "fernando.r@weboperative.com";
-exports.senderName = "Fernando is likewhoa";
+exports.senderEmail = "info@blackboxstocks.com";
+exports.senderName = "Info";
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
-exports.listId = 3548999;
+exports.listId = 4084518;
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
-exports.templateId = "6586ac87-0cc4-4a7b-9e0a-4f947dac5fd6";
+exports.templateId = "5a740de9-a5ba-457f-afad-8b804c02b2a5";
 
 // receive an email when a new signup is confirmed
 exports.sendNotification = true;
-exports.notificationEmail = "design@missionaccomplish.com";
+exports.notificationEmail = "gentooligan@gmail.com";
